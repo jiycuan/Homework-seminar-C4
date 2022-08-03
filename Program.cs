ad431ea83d9dfa3a52b558dbf21983144fd92679
Сорокин Алексей <jiysuan@mail.ru>
2022-08-03 21:30:54 +0400

    Сделал три основных задания и вспомнил как работает гит

--- Program.cs
+++ Program.cs
@@ -1,48 +1,4 @@
-﻿/*
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-
-while (num > 0)
-    {
-        num = num/10;
-        sum++;
-    }
-
-Console.WriteLine(sum);
-*/
-
-// Программа, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
-
-/*
-int [] array = new int [8];
-int i = 0;
-while (i < array.Length) 
-    {
-        int num = new Random().Next(0, 2);
-        array [i] = num;
-        Console.Write(num + " ");
-        i++;
-    }
-Console.WriteLine();
-*/
-
-//Программа, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
-
-/*
-    Console.WriteLine("Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.");
-    Console.WriteLine("Введите число:");
-    int number = Convert.ToInt32(Console.ReadLine());
-    int sum = 1;                    
-    
-    for (int i = 1; i <= number; i++)
-        {
-            sum = sum * i;
-        }
-    Console.WriteLine($"Произведение чисел от 1 до {number} равно {sum}");
-*/
-
-// Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B
+﻿// Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B
 // Степень числа - результат многократного умножения числа на себя.
 
 /*
@@ -77,6 +33,7 @@ Console.WriteLine($"Результат равен {temp}");
 
 // Программа, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив
 
+/*
 int size = 8;
 int[] array = new int[size];
 int temp;
@@ -99,6 +56,7 @@ for (int i = 0; i < size; i++)
             array[a] = array[position];
             array[position] = curr;
 
-    Console.Write($"{array[a]} ");
+    Console.Write($"{array[a]}, ");
     }
 Console.WriteLine();
+*/
